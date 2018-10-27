b6ac4eafad0eac3954c36fb6553b1132bc82069c
Noah Zoschke <noah@segment.com>
2018-10-27 08:17:57 -0700

    switch to ENVOY_CONFIG param

--- bin/envoy.sh
+++ bin/envoy.sh
@@ -2,10 +2,10 @@
 set -ex -o pipefail
 
 ENVOY_CLUSTER=${ENVOY_CLUSTER:-$1}
-ENVOY_OPTS=${ENVOY_OPTS:-"-c /etc/envoy/envoy.yaml"}
+ENVOY_CONFIG=${ENVOY_CONFIG:-/etc/envoy/envoy.yaml}
 
 # run CMD in background
 "$@" &
 
 # run envoy in foreground
-envoy $ENVOY_OPTS --service-node $HOSTNAME --service-cluster $ENVOY_CLUSTER
+envoy $ENVOY_OPTS -c $ENVOY_CONFIG --service-node $HOSTNAME --service-cluster $ENVOY_CLUSTER
